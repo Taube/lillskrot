e66af94c8656a1012aefe7ede911f74f9275124d
Jeanette Taube <jeanette.taube@noaignite.com>
2022-12-01 21:59:46 +0100

    Julsite

---
+++ pages/components/ImageAnimation/ImageAnimation.tsx
@@ -0,0 +1,51 @@
+import Image from 'next/image'
+import { useState } from 'react';
+import styles from "../../jul.module.scss";
+
+interface ImageAnimationProps {
+    imageUrl: string;
+    size: string;
+    xpos: number;
+    ypos: number;
+    text: string;
+    rotate: number;
+}
+
+export const ImageAnimation = (props: ImageAnimationProps) => {
+
+    let startXPos = props.size === "small" ? 450 : props.size === "large" ? 350 : 400;
+    let startYPos = props.size === "small" ? 135 : props.size === "large" ? 0 : 70;
+    const [xPos, setXPos] = useState(startXPos);
+    const [yPos, setYPos] = useState(startYPos);
+    const [rotate, setRotate] = useState(0);
+    
+    setTimeout(() => 
+    {
+        setXPos(props.xpos);
+        setYPos(props.ypos);
+        setRotate(props.rotate);
+      console.log('Initial timeout!')}
+       , 500);
+
+    return (
+        <a 
+            key={props.imageUrl} 
+            href="#" 
+            className={`start ${rotate == 0 ? "rotate" : rotate > 9 ? "rotate-plus10" : "roate-plus5"} ${props.size === "large" ? styles.big : props.size === "medium" ? styles.middle : styles.little } ${styles.polaroid}`} 
+            style={{"top": `${yPos}px`, "left": `${xPos}px`, "transition": "top 2s,left 2s","WebkitTransform": `rotate(${rotate}deg)`, "MozTransformOrigin": `rotate(${rotate}deg)`, "transform": `rotate(${rotate}deg)`}}>
+
+
+
+            {props.size === "small" && (
+                <Image src={props.imageUrl} alt="" height="95" width="85" />
+            )}
+            {props.size === "medium" && (
+                <Image src={props.imageUrl} alt="" height="200" width="170" />
+            )}
+            {props.size === "large" && (
+                <Image src={props.imageUrl} alt="" height="400" width="300" />
+            )}
+            {props.text}
+      </a>
+    );
+};
--- pages/jul2022.tsx
+++ pages/jul2022.tsx
@@ -1,97 +1,203 @@
 import type { NextPage } from "next";
 import styles from "./jul.module.scss";
 import Image from 'next/image'
-import img1 from './/Images/Linnea1.jpg';
-import img2 from './/Images/Linnea2.jpg';
-import img3 from './/Images/jeaben.jpg';
-import img4 from './/Images/balett.jpg';
-import img5 from './/Images/balettpar.jpg';
-import img6 from './/Images/Ellis.jpg';
-import img7 from './/Images/swanlake.jpg';
-import img8 from './/Images/linneaola.jpg';
-import img9 from './/Images/bgschen.jpg';
-import img10 from './/Images/Leo.jpg';
-import img11 from './/Images/skate.png';
-import img12 from './/Images/skridskor.jpg';
-
-import imgA from './/Images/skate.png';
+import { useState } from "react";
+import { ImageAnimation } from "./components/ImageAnimation/ImageAnimation";
 
 const Jul2022: NextPage = () => {
-  // const classNamePic1 = ´${styles.large} ${styles.polaroid} ${styles.img1}´;
+  // const classNamePic1 = ´${styles.large} ${styles.polaroid} ${styles.img1}´;    
+    //var valuesArray = JSON.parse(imgJson1);
+
+    //const [extraClass, setExtraClass] = useState('');
+    
+  
+    const imgJson2 = { "cards": [
+      {
+        "imageUrl": "/imgs/Jul2022/leo3.jpg",
+        "text":"God Jul",
+        "size": "large",
+        "top": 0,
+        "left": 30,
+        "rotate": -10,
+      },
+      {
+        "imageUrl": "/imgs/Jul2022/balett3.jpg",
+        "text":"Inspiration",
+        "size": "medium",
+        "top": 350,
+        "left": 495,
+        "rotate": -20,
+      }, 
+      {
+        "imageUrl": "/imgs/Jul2022/ellis.png",
+        "text":"Ljustekniker",
+        "size": "large",
+        "top": 240,
+        "left": 120,
+        "rotate": 8,
+      }, 
+      {
+        "imageUrl": "/imgs/Jul2022/jeaola_big.png",
+        "text":"Stöd",
+        "size": "medium",
+        "top": 270,
+        "left": 10,
+        "rotate": -8,
+      },
+      {
+        "imageUrl": "/imgs/Jul2022/lampa.jpg",
+        "text":"Lampa",
+        "size": "small",
+        "top": 260,
+        "left": 480,
+        "rotate": -4
+      }, 
+      {
+        "imageUrl": "/imgs/Jul2022/leg2.jpg",
+        "text":"Stöd",
+        "size": "medium",
+        "top": 0,
+        "left": 360,
+        "rotate": -8,
+      }, 
+      {
+        "imageUrl": "/imgs/Jul2022/Linnea2.jpg",
+        "text":"Ballerina",
+        "size": "large",
+        "top": 0,
+        "left": 680,
+        "rotate": 18,
+      },
+      {
+        "imageUrl": "/imgs/Jul2022/Linnea3.jpg",
+        "text":"Smidig",
+        "size": "small",
+        "top": 240,
+        "left": 400,
+        "rotate": 10,
+      },
+      {
+        "imageUrl": "/imgs/Jul2022/Linnea4.jpg",
+        "text":"Håller i benet",
+        "size": "medium",
+        "top": 30,
+        "left": 530,
+        "rotate": -4,
+      },
+      {
+        "imageUrl": "/imgs/Jul2022/ola.jpg",
+        "text":"Stark man",
+        "size": "small",
+        "top": 440,
+        "left": 0,
+        "rotate": -13,
+      },
+      {
+        "imageUrl": "/imgs/Jul2022/skate_black.jpg",
+        "text":"Skridskor",
+        "size": "small",
+        "top": 350,
+        "left": 605,
+        "rotate": -7,
+      },
+      {
+        "imageUrl": "/imgs/Jul2022/skate_white.jpg",
+        "text":"Skridsko",
+        "size": "small",
+        "top": 320,
+        "left": 680,
+        "rotate": 18,
+      },
+      {
+        "imageUrl": "/imgs/Jul2022/leg.jpg",
+        "text":"Ben",
+        "size": "small",
+        "top": 30,
+        "left": 0,
+        "rotate": 8,
+      }, 
+      {
+        "imageUrl": "/imgs/Jul2022/linnis_ola_big.png",
+        "text":"Ben upp",
+        "size": "medium",
+        "top": 400,
+        "left": 700,
+        "rotate": -10,
+      },
+      {
+        "imageUrl": "/imgs/Jul2022/leo.jpg",
+        "text":"Skylt",
+        "size": "medium",
+        "top": 250,
+        "left": 850,
+        "rotate": 0,
+      },
+      {
+        "imageUrl": "/imgs/Jul2022/balett1.jpg",
+        "text":"Inspiration",
+        "size": "small",
+        "top": 160,
+        "left": 990,
+        "rotate": 8
+      },
+      {
+        "imageUrl": "/imgs/Jul2022/balett4.jpg",
+        "text":"Inspiration",
+        "size": "small",
+        "top": 0,
+        "left": 920,
+        "rotate": 8,
+      }, 
+      {
+        "imageUrl": "/imgs/Jul2022/dress.jpg",
+        "text":"Tutu",
+        "size": "small",
+        "top": 460,
+        "left": 330,
+        "rotate": 2,
+      }, 
+      {
+        "imageUrl": "/imgs/Jul2022/balett5.jpg",
+        "text":"Inspiration",
+        "size": "small",
+        "top": 40,
+        "left": 280,
+        "rotate": -1,
+      }, 
+      {
+        "imageUrl": "/imgs/Jul2022/bg.jpg",
+        "text":"Scen",
+        "size": "small",
+        "top": 480,
+        "left": 880,
+        "rotate": 4,
+      }, 
+      {
+        "imageUrl": "/imgs/Jul2022/balett2.jpg",
+        "text":"Inspiration",
+        "size": "small",
+        "top": 500,
+        "left": 400,
+        "rotate": 18,
+      },
+    ]};
 
   return (
     <div className={styles.jul}>
-      <h1>God Jul</h1><br/>
-      <div className={styles.photoAlbum}>
-			<h1><span>The making of 2022...</span></h1>
-      <a href="htts://www.lillskrot.se/" className={`${styles.large} ${styles.polaroid} ${styles.img1}`}>
-        <Image src={img1} alt="" />
-        xxx
-      </a>
-      <a href="htts://www.lillskrot.se/" className={`${styles.small} ${styles.polaroid} ${styles.img3}`}>
-        <Image src={img3} alt="" />
-        xxx
-      </a>
-      <a href="htts://www.lillskrot.se/" className={`${styles.medium} ${styles.polaroid} ${styles.img4}`}>
-        <Image src={img4} alt="" />
-        xxx
-      </a>
-      <a href="htts://www.lillskrot.se/" className={`${styles.large} ${styles.polaroid} ${styles.img10}`}>
-        <Image src={img10} alt="" />
-        xxx
-      </a>
-      <a href="htts://www.lillskrot.se/" className={`${styles.polaroid} ${styles.img5}`}>
-        <Image src={img5} alt="" />
-        xxx
-      </a>
-      <a href="htts://www.lillskrot.se/" className={`${styles.medium} ${styles.polaroid} ${styles.img6}`}>
-        <Image src={img6} alt="" />
-        xxx
-      </a>
-      <a href="htts://www.lillskrot.se/" className={`${styles.polaroid} ${styles.img7}`}>
-        <Image src={img7} alt="" />
-        xxx
-      </a>
-      <a href="htts://www.lillskrot.se/" className={`${styles.polaroid} ${styles.img8}`}>
-        <Image src={img8} alt="" />
-        xxx
-      </a>
-      <a href="htts://www.lillskrot.se/" className={`${styles.medium} ${styles.polaroid} ${styles.img9}`}>
-        <Image src={img9} alt="" />
-        xxx
-      </a>
-      <a href="htts://www.lillskrot.se/" className={`${styles.polaroid} ${styles.img2}`}>
-        <Image src={img2} alt="" />
-        xxx
-      </a>
-      <a href="htts://www.lillskrot.se/" className={`${styles.polaroid} ${styles.imgA}`}>
-        <Image src={imgA} alt="" />
-        xxx
-      </a>
-      
-      <a href="htts://www.lillskrot.se/" className={`${styles.small} ${styles.polaroid} ${styles.img11}`}>
-        <Image src={img11} alt="" />
-        xxx
-      </a>
-      <a href="htts://www.lillskrot.se/" className={`${styles.small} ${styles.polaroid} ${styles.img12}`}>
-        <Image src={img12} alt="" />
-        xxx
-      </a>
-      {/* <a href="htts://www.lillskrot.se/" className={`${styles.small} ${styles.polaroid} ${styles.img13}`}>
-        <Image src={img8} alt="" />
-        xxx
-      </a>
-      <a href="htts://www.lillskrot.se/" className={`${styles.small} ${styles.polaroid} ${styles.img14}`}>
-        <Image src={img8} alt="" />
-        xxx
-      </a>
-      <a href="htts://www.lillskrot.se/" className={`${styles.polaroid} ${styles.img11}`}>
-        <Image src={img8} alt="" />
-        xxx
-      </a> */}
-    </div>
 
-    <h4 className={styles.copyright}>&copy; Lillskrot & Jag</h4>
+      <div className={styles.header}>
+        <h1>God Jul</h1>
+      </div>
+
+      <div className={styles.photoAlbum}>
+        <h1><span>The making of 2022...</span></h1>
+        
+        {imgJson2.cards.map((card, index) => {
+          return <ImageAnimation key={card.imageUrl} imageUrl={card.imageUrl} size={card.size} text={card.text} xpos={card.left} ypos={card.top} rotate={card.rotate}  />
+        })}
+      </div>
+
+      <h4 className={styles.copyright}>&copy; Lillskrot & Jag</h4>
     </div>
 	
   );
