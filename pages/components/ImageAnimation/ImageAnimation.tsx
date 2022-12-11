ce779887fa0988f4377fcf954c5ebec31932121e
Jeanette Taube <jeanette.taube@noaignite.com>
2022-12-11 23:04:57 +0100

    responsive

--- pages/components/ImageAnimation/ImageAnimation.tsx
+++ pages/components/ImageAnimation/ImageAnimation.tsx
@@ -1,8 +1,9 @@
 import Image from 'next/image'
-import { useState } from 'react';
+import {useEffect, useLayoutEffect, useRef, useState} from 'react';
 import styles from "../../jul.module.scss";
 
 interface ImageAnimationProps {
+    width: number;
     imageUrl: string;
     size: string;
     xpos: number;
@@ -13,34 +14,98 @@ interface ImageAnimationProps {
 
 const ImageAnimation = (props: ImageAnimationProps) => {
 
-    let startXPos = props.size === "small" ? 490 : props.size === "large" ? 390 : 440;
-    let startYPos = props.size === "small" ? 135 : props.size === "large" ? 0 : 70;
-    const [xPos, setXPos] = useState(startXPos);
-    const [yPos, setYPos] = useState(startYPos);
-    const [rotate, setRotate] = useState(0);
+    //let startXPos = props.size === "small" ? (props.width - 95) / 2 : props.size === "large" ? (props.width - 300) / 2 : (props.width - 200) / 2;
+    let startXPos = 0;
+    switch(props.size)
+    {
+        case "small": 
+            startXPos = (props.width - 95) / 2;
+            break;
+        case "large":
+            startXPos = (props.width - 300) / 2;
+            break;
+        case "tiny":
+            startXPos = (props.width - 50) / 2;
+            break;
+        default: 
+            startXPos = (props.width - 180) / 2;
+            break;
+    }   
+
+    //let startYPos = props.size === "small" ? 135 : props.size === "large" ? 0 : 70;
+    let startYPos = 0;
+    switch(props.size)
+    {
+        case "small": 
+        startYPos = 135;
+            break;
+        case "large":
+            startYPos = 0;
+            break;
+        case "tiny":
+            startYPos = 155;
+            break;
+        default: 
+            startYPos = 70;
+            break;
+    }
     
+    const [xPos, setXPos] = useState(startXPos);
+    const [yPos, setYPos] = useState(startYPos);    
+    const [rotate, setRotate] = useState(0);
+
     setTimeout(() => 
     {
-        setXPos(props.xpos);
-        setYPos(props.ypos);
+        const newXpos = (props.width / 100 * props.xpos);
+        let newYpos = props.ypos;
+        if (props.width < 400)
+        {
+            if (props.ypos > 300)
+            {
+                newYpos = props.ypos - 100;
+            }
+            else if (props.ypos > 200) {
+                newYpos = props.ypos - 50;
+            }
+        }
+        setXPos(newXpos);
+        setYPos(newYpos);
         setRotate(props.rotate);
-      console.log('Initial timeout!')}
-       , 2500);
+    }, 500);
+
+    let cssSize = "medium";
+    if (props.size === "large") {
+        cssSize = styles.big;
+    } else if (props.size === "largemobile") {
+        cssSize = styles.bigmobile;
+    } else if (props.size === "medium") {
+        cssSize = styles.middle;
+    } else if (props.size === "tiny") {
+            cssSize = styles.tiny;
+    } else {
+        cssSize = styles.little;
+    }
 
     return (
         <a 
             key={props.imageUrl} 
             href="#" 
-            className={`start ${rotate == 0 ? "rotate" : rotate > 9 ? "rotate-plus10" : "roate-plus5"} ${props.size === "large" ? styles.big : props.size === "medium" ? styles.middle : styles.little } ${styles.polaroid}`} 
+            className={`start ${rotate == 0 ? "rotate" : rotate > 9 ? "rotate-plus10" : "roate-plus5"} ${cssSize} ${styles.polaroid}`} 
+            //style={{"top": `${yPos}px`, "left": `${xPos}px`, "transition": "top 2s,left 2s","WebkitTransform": `rotate(${rotate}deg)`, "MozTransformOrigin": `rotate(${rotate}deg)`, "transform": `rotate(${rotate}deg)`}}>
             style={{"top": `${yPos}px`, "left": `${xPos}px`, "transition": "top 2s,left 2s","WebkitTransform": `rotate(${rotate}deg)`, "MozTransformOrigin": `rotate(${rotate}deg)`, "transform": `rotate(${rotate}deg)`}}>
 
 
-
+            {props.size === "tiny" && (
+                <Image src={props.imageUrl} alt="" height="55" width="50" />
+            )}
             {props.size === "small" && (
                 <Image src={props.imageUrl} alt="" height="95" width="85" />
             )}
             {props.size === "medium" && (
                 <Image src={props.imageUrl} alt="" height="200" width="170" />
+            )}
+            {props.size === "largemobile" && (
+                <Image src={props.imageUrl} alt="" height="200" width="150" />
             )}
             {props.size === "large" && (
                 <Image src={props.imageUrl} alt="" height="400" width="300" />
--- pages/jul2022.tsx
+++ pages/jul2022.tsx
@@ -1,185 +1,374 @@
 import type { NextPage } from "next";
 import styles from "./jul.module.scss";
 import Image from 'next/image'
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import ImageAnimation from "./components/ImageAnimation/ImageAnimation";
 
 const Jul2022: NextPage = () => {
-  // const classNamePic1 = ´${styles.large} ${styles.polaroid} ${styles.img1}´;    
-    //var valuesArray = JSON.parse(imgJson1);
-
-    //const [extraClass, setExtraClass] = useState('');
     
   const imgJson2 = { "cards": [
-      {
-        "imageUrl": "/imgs/Jul2022/leo3.jpg",
-        "text":"God Jul",
-        "size": "large",
-        "top": 0,
-        "left": 30,
-        "rotate": -10,
-      },
-      {
+      //     {
+  //       "imageUrl": "/imgs/Jul2022/leo3.jpg",
+  //       "text":"God Jul",
+  //       "size": "large",
+  //       "top": 0,
+  //       "left": 30,
+  //       "rotate": -10,
+  //     },
+       {
+         "imageUrl": "/imgs/Jul2022/leo3.jpg",
+         "text":"God Jul",
+         "size": "large",
+         "top": 0,
+         "left": 4,
+         "rotate": -9,
+       },
+         //     {
+  //       "imageUrl": "/imgs/Jul2022/balett3.jpg",
+  //       "text":"Inspiration",
+  //       "size": "medium",
+  //       "top": 350,
+  //       "left": 495,
+  //       "rotate": -20,
+  //     }, 
+       {
         "imageUrl": "/imgs/Jul2022/balett3.jpg",
         "text":"Inspiration",
         "size": "medium",
         "top": 350,
-        "left": 495,
+        "left": 46,
         "rotate": -20,
       }, 
-      {
+  //     {
+  //       "imageUrl": "/imgs/Jul2022/ellis.png",
+  //       "text":"Ljustekniker",
+  //       "size": "large",
+  //       "top": 240,
+  //       "left": 120,
+  //       "rotate": 8,
+  //     }, 
+        {
         "imageUrl": "/imgs/Jul2022/ellis.png",
         "text":"Ljustekniker",
         "size": "large",
         "top": 240,
-        "left": 120,
+        "left": 10,
         "rotate": 8,
       }, 
-      {
+  //     {
+  //       "imageUrl": "/imgs/Jul2022/jeaola_big.png",
+  //       "text":"Stöd",
+  //       "size": "medium",
+  //       "top": 270,
+  //       "left": 10,
+  //       "rotate": -8,
+  //     },
+        {
         "imageUrl": "/imgs/Jul2022/jeaola_big.png",
         "text":"Stöd",
         "size": "medium",
         "top": 270,
-        "left": 10,
+        "left": 3,
         "rotate": -8,
       },
+  //     {
+  //       "imageUrl": "/imgs/Jul2022/lampa.jpg",
+  //       "text":"Lampa",
+  //       "size": "small",
+  //       "top": 260,
+  //       "left": 480,
+  //       "rotate": -4
+  //     }, 
       {
         "imageUrl": "/imgs/Jul2022/lampa.jpg",
         "text":"Lampa",
         "size": "small",
         "top": 260,
-        "left": 480,
+        "left": 45,
         "rotate": -4
       }, 
+  //     {
+  //       "imageUrl": "/imgs/Jul2022/leg2.jpg",
+  //       "text":"Stöd",
+  //       "size": "medium",
+  //       "top": 0,
+  //       "left": 360,
+  //       "rotate": -8,
+  //     }, 
       {
         "imageUrl": "/imgs/Jul2022/leg2.jpg",
         "text":"Stöd",
         "size": "medium",
         "top": 0,
-        "left": 360,
+        "left": 30,
         "rotate": -8,
       }, 
-      {
+  //     {
+  //       "imageUrl": "/imgs/Jul2022/linnea2.jpg",
+  //       "text":"Ballerina",
+  //       "size": "large",
+  //       "top": 0,
+  //       "left": 700,
+  //       "rotate": 18,
+  //     },
+        {
         "imageUrl": "/imgs/Jul2022/linnea2.jpg",
         "text":"Ballerina",
         "size": "large",
         "top": 0,
-        "left": 700,
-        "rotate": 18,
+        "left": 55,
+        "rotate": 14,
       },
-      {
+  //     {
+  //       "imageUrl": "/imgs/Jul2022/linnea3.jpg",
+  //       "text":"Smidig",
+  //       "size": "small",
+  //       "top": 240,
+  //       "left": 400,
+  //       "rotate": 10,
+  //     },
+        {
         "imageUrl": "/imgs/Jul2022/linnea3.jpg",
         "text":"Smidig",
         "size": "small",
         "top": 240,
-        "left": 400,
+        "left": 37,
         "rotate": 10,
       },
-      {
+  //     {
+  //       "imageUrl": "/imgs/Jul2022/linnea4.jpg",
+  //       "text":"Håller i benet",
+  //       "size": "medium",
+  //       "top": 30,
+  //       "left": 530,
+  //       "rotate": -4,
+  //     },
+        {
         "imageUrl": "/imgs/Jul2022/linnea4.jpg",
         "text":"Håller i benet",
         "size": "medium",
         "top": 30,
-        "left": 530,
+        "left": 50,
         "rotate": -4,
       },
-      {
+  //     {
+  //       "imageUrl": "/imgs/Jul2022/ola.jpg",
+  //       "text":"Stark man",
+  //       "size": "small",
+  //       "top": 440,
+  //       "left": 0,
+  //       "rotate": -13,
+  //     },
+        {
         "imageUrl": "/imgs/Jul2022/ola.jpg",
         "text":"Stark man",
         "size": "small",
         "top": 440,
-        "left": 0,
+        "left": 3,
         "rotate": -13,
       },
-      {
+  //     {
+  //       "imageUrl": "/imgs/Jul2022/skate_black.jpg",
+  //       "text":"Skridskor",
+  //       "size": "small",
+  //       "top": 350,
+  //       "left": 605,
+  //       "rotate": -7,
+  //     },
+        {
         "imageUrl": "/imgs/Jul2022/skate_black.jpg",
         "text":"Skridskor",
         "size": "small",
         "top": 350,
-        "left": 605,
+        "left": 57,
         "rotate": -7,
       },
-      {
+  //     {
+  //       "imageUrl": "/imgs/Jul2022/skate_white.jpg",
+  //       "text":"Skridsko",
+  //       "size": "small",
+  //       "top": 320,
+  //       "left": 680,
+  //       "rotate": 18,
+  //     },
+        {
         "imageUrl": "/imgs/Jul2022/skate_white.jpg",
         "text":"Skridsko",
         "size": "small",
         "top": 320,
-        "left": 680,
+        "left": 64,
         "rotate": 18,
       },
-      {
+  //     {
+  //       "imageUrl": "/imgs/Jul2022/leg.jpg",
+  //       "text":"Ben",
+  //       "size": "small",
+  //       "top": 30,
+  //       "left": 0,
+  //       "rotate": 8,
+  //     }, 
+        {
         "imageUrl": "/imgs/Jul2022/leg.jpg",
         "text":"Ben",
         "size": "small",
         "top": 30,
-        "left": 0,
+        "left": 2,
         "rotate": 8,
       }, 
-      {
+  //     {
+  //       "imageUrl": "/imgs/Jul2022/linnis_ola_big.png",
+  //       "text":"Ben upp",
+  //       "size": "medium",
+  //       "top": 400,
+  //       "left": 700,
+  //       "rotate": -10,
+  //     },
+        {
         "imageUrl": "/imgs/Jul2022/linnis_ola_big.png",
         "text":"Ben upp",
         "size": "medium",
         "top": 400,
-        "left": 700,
+        "left": 65,
         "rotate": -10,
       },
-      {
+  //     {
+  //       "imageUrl": "/imgs/Jul2022/leo.jpg",
+  //       "text":"Skylt",
+  //       "size": "medium",
+  //       "top": 250,
+  //       "left": 870,
+  //       "rotate": 0,
+  //     },
+        {
         "imageUrl": "/imgs/Jul2022/leo.jpg",
         "text":"Skylt",
         "size": "medium",
         "top": 250,
-        "left": 870,
+        "left": 72,
         "rotate": 0,
       },
-      {
+  //     {
+  //       "imageUrl": "/imgs/Jul2022/balett1.jpg",
+  //       "text":"Inspiration",
+  //       "size": "small",
+  //       "top": 160,
+  //       "left": 1000,
+  //       "rotate": 8
+  //     },
+        {
         "imageUrl": "/imgs/Jul2022/balett1.jpg",
         "text":"Inspiration",
         "size": "small",
         "top": 160,
-        "left": 1000,
+        "left": 82,
         "rotate": 8
       },
-      {
+  //     {
+  //       "imageUrl": "/imgs/Jul2022/balett4.jpg",
+  //       "text":"Inspiration",
+  //       "size": "small",
+  //       "top": 0,
+  //       "left": 940,
+  //       "rotate": 8,
+  //     }, 
+        {
         "imageUrl": "/imgs/Jul2022/balett4.jpg",
         "text":"Inspiration",
         "size": "small",
         "top": 0,
-        "left": 940,
+        "left": 82,
         "rotate": 8,
       }, 
-      {
+  //     {
+  //       "imageUrl": "/imgs/Jul2022/dress.jpg",
+  //       "text":"Tutu",
+  //       "size": "small",
+  //       "top": 460,
+  //       "left": 330,
+  //       "rotate": 2,
+  //     }, 
+        {
         "imageUrl": "/imgs/Jul2022/dress.jpg",
         "text":"Tutu",
         "size": "small",
         "top": 460,
-        "left": 330,
+        "left": 29,
         "rotate": 2,
       }, 
-      {
+  //     {
+  //       "imageUrl": "/imgs/Jul2022/balett5.jpg",
+  //       "text":"Inspiration",
+  //       "size": "small",
+  //       "top": 40,
+  //       "left": 280,
+  //       "rotate": -1,
+  //     }, 
+        {
         "imageUrl": "/imgs/Jul2022/balett5.jpg",
         "text":"Inspiration",
         "size": "small",
         "top": 40,
-        "left": 280,
+        "left": 24,
         "rotate": -1,
       }, 
-      {
+  //     {
+  //       "imageUrl": "/imgs/Jul2022/bg.jpg",
+  //       "text":"Scen",
+  //       "size": "small",
+  //       "top": 480,
+  //       "left": 880,
+  //       "rotate": 4,
+  //     }, 
+        {
         "imageUrl": "/imgs/Jul2022/bg.jpg",
         "text":"Scen",
         "size": "small",
         "top": 480,
-        "left": 880,
+        "left": 84,
         "rotate": 4,
       }, 
-      {
+  //     {
+  //       "imageUrl": "/imgs/Jul2022/balett2.jpg",
+  //       "text":"Inspiration",
+  //       "size": "small",
+  //       "top": 500,
+  //       "left": 400,
+  //       "rotate": 17,
+  //     },
+        {
         "imageUrl": "/imgs/Jul2022/balett2.jpg",
         "text":"Inspiration",
         "size": "small",
         "top": 500,
-        "left": 400,
+        "left": 36,
         "rotate": 17,
       },
-    ]};
+     ]};
+
+     const [dimensions, setDimensions] = useState({
+      height: 0,
+      width: 0
+    });
+
+
+  useEffect(() => {
+      setDimensions({
+          height: window.innerHeight,
+          width: window.innerWidth
+  });
+
+  function handleResize() {
+    setDimensions({
+      width: window.innerWidth,
+      height: window.innerHeight
+    });
+  }
+  window.addEventListener("resize", handleResize);
+}, []);
+
+console.log("dimensions.width", dimensions.width);
+
+
 
   return (
     <div className={styles.jul}>
@@ -191,8 +380,26 @@ const Jul2022: NextPage = () => {
       <div className={styles.photoAlbum}>
         <h1><span>The making of 2022...</span></h1>
         
-        {imgJson2.cards.map((card, index) => {
-          return <ImageAnimation key={card.imageUrl} imageUrl={card.imageUrl} size={card.size} text={card.text} xpos={card.left} ypos={card.top} rotate={card.rotate}  />
+        {dimensions.width > 0 && imgJson2.cards.map((card, index) => {
+          let cardSize = card.size;
+          if (dimensions.width < 700)
+          {
+            console.log("cardSize", dimensions.width);
+            if (card.size === "large")
+            {
+              cardSize = "largemobile";
+            }
+            else if (card.size == "medium")
+            {
+              cardSize = "small"
+            }
+            else if (card.size == "small")
+            {
+              cardSize = "tiny"
+            }
+          }
+          console.log("cardSize", cardSize, card.imageUrl);
+          return <ImageAnimation width={dimensions.width} key={card.imageUrl} imageUrl={card.imageUrl} size={cardSize} text={card.text} xpos={card.left} ypos={card.top} rotate={card.rotate}  />
         })}
       </div>
 
